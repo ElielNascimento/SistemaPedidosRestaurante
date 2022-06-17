f08d9ac8cd24fc093ebcb122dc7f6a520c04746c
ElielNascimento <nascimentob.eliel@gmail.com>
2022-06-17 19:05:57 -0300

    criação do projeto

---
+++ productOrder/product-app/src/test/java/br/api/java/productapp/ProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.api.java.productapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
